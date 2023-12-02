73262a331be267bcb16d530b48d20a981a5c70c5
karishmarajput <karrajput3948@gmail.com>
2023-12-02 17:04:35 +0530

    og dashboard

---
+++ client/src/pages/organisation/GenerateCertificate.js
@@ -0,0 +1,9 @@
+function GenerateCertificate(){
+    console.log('yo')
+    return(
+        <div>
+            GenerateCertificate
+        </div>
+    )
+}
+export default GenerateCertificate
--- client/src/pages/organisation/OrganisationDashboard.js
+++ client/src/pages/organisation/OrganisationDashboard.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import GenerateCertificate from './GenerateCertificate';
+
 function OrganisationDashboard(){
+    const [loggedIn, setLoggedIn] = useState(false);
+    useEffect(() => {
+        const authToken = localStorage.getItem('authToken');
+        if (authToken) {
+          setLoggedIn(true);
+        }
+      }, []);
+    const handleGenerateCertificate = () => {
+      console.log('hello')
+        // Check if logged in before rendering GenerateCertificate component
+        if (loggedIn) {
+          console.log("hi")
+          // Render the GenerateCertificate component
+          return <GenerateCertificate />;
+        } else {
+          // User not logged in, show a message or redirect to login page
+          return <p>Please login to generate a certificate</p>;
+        }
+      };
     return(
-        <div>hi</div>
+        <div>
+
+            Dashboard
+            <button onClick={handleGenerateCertificate}>Generate Certificate</button>
+        </div>
     )
 }
 export default OrganisationDashboard
